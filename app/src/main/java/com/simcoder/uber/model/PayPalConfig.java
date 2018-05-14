9de3bf64adb05aac0cc91dcb42f2ffee629f27b2
truongvanha <hachange.sgroup@gmail.com>
2018-05-14 09:22:55 +0700

    Initial commit

---
+++ app/src/main/java/com/simcoder/uber/model/PayPalConfig.java
@@ -0,0 +1,11 @@
+package com.simcoder.uber.model;
+
+/**
+ * Created by Ha Truong on 4/15/2018.
+ * This is a HappyRider
+ * into the com.simcoder.uber.model
+ */
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "";
+}
